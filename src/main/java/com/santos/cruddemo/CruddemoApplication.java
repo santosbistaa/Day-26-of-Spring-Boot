8df0cf02c1b9da64b45b16ef1aed5c29b5d71ddd
Santosh Bista <santosbista17@gmail.com>
2025-03-27 16:06:12 +0545

    Uni-Directional

--- src/main/java/com/santos/cruddemo/CruddemoApplication.java
+++ src/main/java/com/santos/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.santos.cruddemo;
 
+import com.santos.cruddemo.dao.AppDAO;
+import com.santos.cruddemo.entity.Instructor;
+import com.santos.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +16,69 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 		return runner-> {
-				System.out.println("Hello World");
+				//createInstructor(appDAO);
+
+			//findInstructor(appDAO);
+
+			deleteInstructor(appDAO);
 		};
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+
+		int theId = 3;
+		System.out.println("Deleting instructor id: " + theId);
+
+		appDAO.deleteInstructorById(theId);
+
+		System.out.println("Done!");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+
+		int theId = 2;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associate instructorDetail only: " +tempInstructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+/*
+		// create the instructor
+		Instructor tempInstructor =
+				new Instructor("Santos","Bista","santos@gmail.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail("http://www.santos.com/youtube",
+						"Coding!!!");
+*/
+
+		// create the instructor
+		Instructor tempInstructor =
+				new Instructor("Sagar","Thapa","sagar@gmail.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail("http://www.sagar.com/youtube",
+						"Guitar!!!");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor
+		//
+ 		// NOTE: this will Also save the details object
+		// because of CascadeType.ALL
+		//
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!");
 	}
 
 }
---
+++ src/main/java/com/santos/cruddemo/dao/AppDAO.java
@@ -0,0 +1,13 @@
+package com.santos.cruddemo.dao;
+
+import com.santos.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+}
